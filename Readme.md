db7aefa694e49bedcbfd13a5b9585da087eca197
Sahil <leviack629@gmail.com>
2023-11-26 12:49:55 +0530

    setup configured

--- Readme.md
+++ Readme.md
@@ -3,3 +3,5 @@
 this is practice backend project with javascript
 
 [Model link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
+
+ 
---
+++ src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controller.js";
+
+const router = Router();
+
+
+//register route
+router.route("/register").post(registerUser)
+
+export default router;
